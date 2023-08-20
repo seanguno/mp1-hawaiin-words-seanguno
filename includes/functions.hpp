668fd19192d4d93eb0ac51d3bf3a53a962d58c1a
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-08-19 21:36:24 -0500

    Update functions.hpp

--- includes/functions.hpp
+++ includes/functions.hpp
@@ -1,6 +1,22 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
+#include <string>
 
-// Your function declarations should go in this header file.
+
+std::string CheckA(std::string str, unsigned int i);
+std::string CheckE(std::string str, unsigned int i);
+std::string CheckW(std::string str, unsigned int i);
+std::string CheckO(std::string str, unsigned int i);
+std::string CheckI(std::string str, unsigned int i);
+std::string CheckU(std::string str, unsigned int i);
+std::string ToLower(std::string str);
+bool IsDash(std::string str, unsigned int i);
+bool IsVowel(char is_vowel);
+bool IsValid(char is_valid);
+std::string CheckVowel(std::string str, unsigned int i);
+std::string ReturnDefault(char default_char);
+std::string CharToString(char to_string);
+
+
 
 #endif
